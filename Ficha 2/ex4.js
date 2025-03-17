905af448f055d530c51d8acb1c785796865eb406
Miguyy <miguelangelo123machado@gmail.com>
2025-03-17 14:36:15 +0000

    corrigido

--- Ficha 2/ex4.js
+++ Ficha 2/ex4.js
@@ -1,14 +1,14 @@
 let num = +prompt("Indique um numero inteiro para a tabuada: ")
 let tabuada
 
+if (num = " "){
+    num=10
+}
+
 function showTabuada(){
     for (let i = 1; i<=10;i++){
         tabuada = num * i
         console.log(tabuada)
-        if (num = " "){
-            tabuada = 10 * i
-            console.log(tabuada)
-        }
     }
 }
 
